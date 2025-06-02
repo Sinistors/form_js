b1db008b2313c292002e68915a54c46232dbc12b
Nicolas P <nicolas.puissant.pro@gmail.com>
2025-06-02 11:14:31 +0200

    fin exo + style (pas beau)

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let form = document.querySelector("#form");
 
 form.addEventListener("submit", (e) =>
 {
-    console.log("ui");
     e.preventDefault();
 
 
-    let container = document.createElement("div");
+    let container = document.querySelector("#container");
+    container.innerHTML = "";
 
     let name = document.createElement("p");
     name.innerHTML = "<span class=\"bold\">Nom : </span> " + document.querySelector("#name").value;
@@ -14,12 +14,46 @@ form.addEventListener("submit", (e) =>
     let firstName = document.createElement("p");
     firstName.innerHTML = "<span class=\"bold\">Prenom :</span> " + document.querySelector("#firstName").value;
     container.append(firstName);
+    let age = document.createElement("p");
+    age.innerHTML = "<span class=\"bold\">Age :</span> " + document.querySelector("#age").value + " ans";
+    container.append(age);
+
+    let genderRadios = document.getElementsByName("gender");
+    let gender = document.createElement("p");
+    genderRadios.forEach(element => 
+    {
+        if (element.checked)
+        {
+            gender.innerHTML = "<span class=\"bold\">Genre :</span> " + element.value;
+        } 
+    });
+    container.append(gender);
+
+    let job = document.createElement("p");
+    job.innerHTML = "<span class=\"bold\">Metier :</span> " + document.querySelector("#job").value;
+    container.append(job);
+
+
+    let hobbyCheckboxes = document.getElementsByName("hobby");
+    let hobbies = document.createElement("p");
+    hobbies.innerHTML = "<span class=\"bold\">Hobbies :</span> ";
+    hobbyCheckboxes.forEach(element => 
+    {
+        if (element.checked)
+        {
+            hobbies.innerHTML += " " + element.value;
+        } 
+    });
+    container.append(hobbies);
+
+    let region = document.createElement("p");
+    region.innerHTML = "<span class=\"bold\">Region :</span> " + document.querySelector("#region").value;
+    container.append(region);
+
+    let linkedinLink = document.createElement("p");
+    linkedinLink.innerHTML = "<span class=\"bold\">LinkedIn :</span> <a href=\"\">" + document.querySelector("#linkedin").value + "</a>";
+    container.append(linkedinLink);
 
 
 
-
-
-
-    document.querySelector("body").append(container);
-
-})
+});
